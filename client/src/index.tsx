18e5fcceb5dfed86159ead24b15d883a7a514f4a
Volodymyr Kovalchuk <volodymyr.kovalchuk@atos.net>
2019-04-01 13:23:20 +0200

    added redux devtool

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import createSagaMiddleware from 'redux-saga';
 import thunk from 'redux-thunk';
-// import dotenv from 'dotenv';
 
 import './index.css';
 import App from './App';
@@ -12,9 +11,10 @@ import * as serviceWorker from './serviceWorker';
 import reducer from "./store/reducers/app";
 import { watchApp } from './store/sagas/index';
 
-// dotenv.config();
+// @ts-ignore
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const sagaMiddleware = createSagaMiddleware();
-const store = createStore(reducer, applyMiddleware(thunk, sagaMiddleware));
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk, sagaMiddleware)));
 
 sagaMiddleware.run(watchApp);
 
